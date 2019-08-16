42b1562227be428ceec10866ad39146c42b46000
elihimas <arthurelihimas@gmail.com>
2019-08-16 00:29:28 -0300

    Fixed grid images layout

--- app/src/main/java/com/elihimas/moviedatabase/adapters/MoviesAdapter.kt
+++ app/src/main/java/com/elihimas/moviedatabase/adapters/MoviesAdapter.kt
@@ -6,13 +6,14 @@ import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.elihimas.moviedatabase.R
 import com.elihimas.moviedatabase.activities.MovieDetailsActivity
 import com.elihimas.moviedatabase.model.Movie
 import kotlinx.android.synthetic.main.movie_item.view.*
 
-const val IMAGES_URL = "https://image.tmdb.org/t/p/w342/"
+const val IMAGES_URL = "https://image.tmdb.org/t/p/w500/"
 
 class MoviesAdapter : PagedListAdapter<Movie, MovieViewHolder>(MoviesDiffCallback()) {
 
@@ -23,7 +24,7 @@ class MoviesAdapter : PagedListAdapter<Movie, MovieViewHolder>(MoviesDiffCallbac
             MovieDetailsActivity.start(view.context, movie.id, movie.title)
         }
 
-        return MovieViewHolder(view, 5f, 30f)
+        return MovieViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
@@ -33,21 +34,15 @@ class MoviesAdapter : PagedListAdapter<Movie, MovieViewHolder>(MoviesDiffCallbac
     }
 }
 
-class MovieViewHolder(private val view: View, private val strokeWidth: Float, private val centerRadius: Float) :
+class MovieViewHolder(private val view: View) :
     RecyclerView.ViewHolder(view) {
     fun bind(movie: Movie) {
         view.movie_title_text.text = movie.title
-
-//        val circularProgressDrawable = CircularProgressDrawable(view.context)
-//        circularProgressDrawable.strokeWidth = strokeWidth
-//        circularProgressDrawable.centerRadius = centerRadius
-//        circularProgressDrawable.start()
 
         val imageUrl = IMAGES_URL + movie.posterPath
         Glide.with(view.context)
             .load(imageUrl)
             .centerCrop()
-//            .placeholder(circularProgressDrawable)
             .into(view.poster_image)
         view.tag = movie
     }
